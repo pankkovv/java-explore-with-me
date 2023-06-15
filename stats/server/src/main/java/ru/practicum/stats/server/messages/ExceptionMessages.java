15f4a3e8cd4fd670c5a71212eee352bf118f88be
Панков Денис <pankkovv@yandex.ru>
2023-06-15 19:49:39 +0300

    feat: create a new stack message for stats module

---
+++ stats/server/src/main/java/ru/practicum/stats/server/messages/ExceptionMessages.java
@@ -0,0 +1,10 @@
+package ru.practicum.stats.server.messages;
+
+public enum ExceptionMessages {
+
+//    public final String label;
+//
+//    ExceptionMessages(String label) {
+//        this.label = label;
+//    }
+}
---
+++ stats/server/src/main/java/ru/practicum/stats/server/messages/LogMessages.java
@@ -0,0 +1,10 @@
+package ru.practicum.stats.server.messages;
+
+public enum LogMessages {
+
+//    public final String label;
+//
+//    LogMessages(String label) {
+//        this.label = label;
+//    }
+}
