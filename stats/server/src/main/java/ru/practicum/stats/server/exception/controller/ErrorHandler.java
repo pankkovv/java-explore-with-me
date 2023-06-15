699854c743369ccc7ac71db2eab949bef4e9b231
Панков Денис <pankkovv@yandex.ru>
2023-06-15 19:49:17 +0300

    feat: create a new error handling block

---
+++ stats/server/src/main/java/ru/practicum/stats/server/exception/controller/ErrorHandler.java
@@ -0,0 +1,10 @@
+package ru.practicum.stats.server.exception.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+}
