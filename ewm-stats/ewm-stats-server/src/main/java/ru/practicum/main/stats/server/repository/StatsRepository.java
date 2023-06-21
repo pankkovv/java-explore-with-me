61e2353b860558340e5af574d7116895f13ce890
Панков Денис <Pankkovv@yandex.ru>
2023-06-21 12:48:37 +0300

    feat: transform methods find stats on uri

--- ewm-stats/ewm-stats-server/src/main/java/ru/practicum/main/stats/server/repository/StatsRepository.java
+++ ewm-stats/ewm-stats-server/src/main/java/ru/practicum/main/stats/server/repository/StatsRepository.java
@@ -17,7 +17,7 @@ public interface StatsRepository extends JpaRepository<Stats, Long> {
     @Query("select new ru.practicum.main.stats.dto.ResponseDto(s.app, s.uri, count(s.app)) " +
             "from Stats as s " +
             "where s.timestamp between ?1 and ?2 " +
-            "and lower(?3) like lower(s.uri) " +
+            "and lower(s.uri) like lower('%/' + ?3 + '%') " +
             "group by s.app, s.uri " +
             "having count(s.ip) = 1 " +
             "order by count(s.app) desc ")
@@ -26,7 +26,7 @@ public interface StatsRepository extends JpaRepository<Stats, Long> {
     @Query("select new ru.practicum.main.stats.dto.ResponseDto(s.app, s.uri, count(s.app))" +
             "from Stats as s " +
             "where s.timestamp between ?1 and ?2 " +
-            "and lower(?3) like lower(s.uri) " +
+            "and lower(s.uri) like lower('%/' + ?3 + '%') " +
             "group by s.app, s.uri " +
             "order by count(s.app) desc ")
     List<ResponseDto> findStatUri(LocalDateTime start, LocalDateTime end, String uri);
